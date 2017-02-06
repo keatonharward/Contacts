ddcb048969872af407418ae9e7e61aed9f5d2671
Keaton Harward <keatonharward@Keatons-MacBook-Pro-2.local>
2017-02-06 16:13:53 -0700

    added Person class, deleted fake property

---
+++ Contacts/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  Contacts
+//
+//  Created by Keaton Harward on 2/6/17.
+//  Copyright © 2017 KeatonHarward. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    init() {
+        print("done")
+    }
+}
